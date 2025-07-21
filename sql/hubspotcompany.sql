1f16fabc6154ae4775bce1c58a94a1bfa6de4f88
lachezargergov <lachezar.gergov@sentricmusic.com>
2025-07-21 12:01:47 +0100

    some minor changes

--- sql/hubspotcompany.sql
+++ sql/hubspotcompany.sql
@@ -2,12 +2,7 @@ CREATE OR REPLACE VIEW "hubspotcompany" AS
 SELECT
   "company"."id" "companyid"
 , "company"."property_nr_client_type" "clienttype_nr"
-<<<<<<< HEAD
 , "company"."property_service_level" "servicelevel"
-, "company"."property_genre" "genre"
-=======
-, "company"."property_client_type" "clienttier"
->>>>>>> test8
 , DATE("company"."property_start_date") "contractstartdate"
 , DATE("company"."property_last_renewal_date") "lastrenewaldate"
 , "company"."property_terminated" "terminated"
